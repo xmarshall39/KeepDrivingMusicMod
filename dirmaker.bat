aa41119074342a8782a15ba3d2767fae746aa4dd
Xavier Marshall <48485277+xmarshall39@users.noreply.github.com>
2025-05-09 20:51:07 -0400

    Update dirmaker.bat

--- dirmaker.bat
+++ dirmaker.bat
@@ -1,12 +1,102 @@
+if not exist "Westkust_" mkdir Westkust_
 if not exist "HolyNow_" mkdir HolyNow_
 if not exist "Zimmer_" mkdir Zimmer_
 if not exist "Mundane_" mkdir Mundane_
 if not exist "CrystalBoys_" mkdir CrystalBoys_
 if not exist "FWA_" mkdir FWA_
-if not exist "Makthaverskan" mkdir Makthaverskan
+if not exist "Makthaverskan_" mkdir Makthaverskan_
 if not exist "Honeydrips_" mkdir Honeydrips_
 if not exist "Dorena_" mkdir Dorena_
 if not exist "MyDarlingYou_" mkdir MyDarlingYou_
 if not exist "Aasma_" mkdir Aasma_
 if not exist "Songwriter_" mkdir Songwriter_
 if not exist "Huervo_" mkdir Huervo_
+if not exist "Feral_" mkdir Feral_
+
+@echo off
+echo 1 Swirl > Westkust_/songlist.txt
+echo 2 Dishwasher >> Westkust_/songlist.txt
+echo 3 Drown >> Westkust_/songlist.txt
+echo 4 0700 >> Westkust_/songlist.txt
+echo 5 Weekends >> Westkust_/songlist.txt
+echo 6 Easy >> Westkust_/songlist.txt
+echo 7 Jonna >> Westkust_/songlist.txt
+echo 8 Summer 3D >> Westkust_/songlist.txt
+echo 9 Another Day >> Westkust_/songlist.txt
+echo 10 Weekends >> Westkust_/songlist.txt
+
+
+echo 1 Wake Up > HolyNow_/songlist.txt
+echo 2 Ballad >> HolyNow_/songlist.txt
+echo 3 Places >> HolyNow_/songlist.txt
+echo 4 Dream of Me >> HolyNow_/songlist.txt
+
+echo 1 Famous Chinese Short Stories > Zimmer_/songlist.txt
+echo 2 Dog in the Sun >> Zimmer_/songlist.txt
+echo 3 Cowboy Pills >> Zimmer_/songlist.txt
+echo 4 Ive Been Wasting All My Good Years >> Zimmer_/songlist.txt
+echo 5 Hometowns >> Zimmer_/songlist.txt
+echo 6 Let Me Down >> Zimmer_/songlist.txt
+
+echo 1 Riff Raff > Mundane_/songlist.txt
+echo 2 Fast but Lazy >> Mundane_/songlist.txt
+echo 3 Never Change >> Mundane_/songlist.txt
+echo 4 New Friends >> Mundane_/songlist.txt
+echo Summer Day >> Mundane_/songlist.txt
+
+echo 1 Nightlife > CrystalBoys_/songlist.txt
+echo 2 Gold >> CrystalBoys_/songlist.txt
+echo 3 Tigers >> CrystalBoys_/songlist.txt
+echo 4 Break Away >> CrystalBoys_/songlist.txt
+echo 5 Heat Waves >> CrystalBoys_/songlist.txt
+echo 6 Color of Snow >> CrystalBoys_/songlist.txt
+echo 7 Lost Times >> CrystalBoys_/songlist.txt
+echo 8 End and Begin >> CrystalBoys_/songlist.txt
+echo 9 Endless Ocean >> CrystalBoys_/songlist.txt
+echo 10 Close Your Eyes >> CrystalBoys_/songlist.txt
+
+echo 1 The Tito Beltran Massacre > FWA_/songlist.txt
+echo 2 Boy Makes Wolf >> FWA_/songlist.txt
+echo 3 Your Ex Doesn't Mark Treasures >> FWA_/songlist.txt
+echo 4 Eva Spence >> FWA_/songlist.txt
+echo 5 Helen Tiger >> FWA_/songlist.txt
+echo 6 Yasuko Onuki >> FWA_/songlist.txt
+echo 7 Jemina Pearl >> FWA_/songlist.txt
+
+echo 1 Leda > Makthaverskan_/songlist.txt
+echo 2 Witness >> Makthaverskan_/songlist.txt
+echo 3 Antabus >> Makthaverskan_/songlist.txt
+echo 4 Asleep >> Makthaverskan_/songlist.txt
+echo 5 Outshine >> Makthaverskan_/songlist.txt
+echo 6 Distance >> Makthaverskan_/songlist.txt
+
+echo 1 Fatima Says > Honeydrips_/songlist.txt
+echo 2 Bright >> Honeydrips_/songlist.txt
+echo 3 Fire We Light >> Honeydrips_/songlist.txt
+echo 4 In The City >> Honeydrips_/songlist.txt
+echo 5 There Comes A Time >> Honeydrips_/songlist.txt
+
+echo 1 Dandelion > Dorena_/songlist.txt
+
+echo 1 Arkenbuzzsaw > Feral_/songlist.txt
+
+echo 1 Midsummer Party > MyDarlingYou_/songlist.txt
+echo 2 It's Not Nice To Be Alone In The Fall >> MyDarlingYou_/songlist.txt
+echo 3 This Weather Pisses Me Off >> MyDarlingYou_/songlist.txt
+echo 4 Hungry and Alone >> MyDarlingYou_/songlist.txt
+echo 5 Last4ever >> MyDarlingYou_/songlist.txt
+echo 6 We Break Up On Friday >> MyDarlingYou_/songlist.txt
+echo 7 Please Don't Talk To Me I Fall In Love So Easily >> MyDarlingYou_/songlist.txt
+echo 8 Everything, Alright >> MyDarlingYou_/songlist.txt
+echo 9 Cigarettes and Peoplesbeer >> MyDarlingYou_/songlist.txt
+echo 10 Friday >> MyDarlingYou_/songlist.txt
+
+echo 1 Going Nowhere > Aasma_/songlist.txt
+
+echo 1 How Many Nights > Songwriter_/songlist.txt
+echo 2 Lord >> Songwriter_/songlist.txt
+echo 3 You Won You Won You Won You Won >> Songwriter_/songlist.txt
+
+echo 1 FIVE > Huervo_/songlist.txt
+echo 2 Recluse >> Huervo_/songlist.txt
+
